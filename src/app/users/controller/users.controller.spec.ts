b7f34f07ddda0dd5da195e50a284028d76c807ec
Paul Vincent Contreras <vincent.contreras@outlook.com>
2021-11-17 15:18:40 +0900

    fix: make user controller test runnable

--- src/app/users/controller/users.controller.spec.ts
+++ src/app/users/controller/users.controller.spec.ts
@@ -1,15 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../service/users.service';
 import { UsersController } from './users.controller';
+
+jest.mock('../service/users.service');
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController]
+      controllers: [UsersController],
+      providers: [UsersService]
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
