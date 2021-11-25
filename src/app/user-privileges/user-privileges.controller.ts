258d503531d4f85dfeb197999fc24cd017737631
Paul Vincent Contreras <vincent.contreras@outlook.com>
2021-11-25 16:22:40 +0900

    feat: add documentations for swagger

--- src/app/user-privileges/user-privileges.controller.ts
+++ src/app/user-privileges/user-privileges.controller.ts
@@ -10,7 +10,8 @@ import {
 import { UserPrivilegesService } from "./user-privileges.service";
 import { CreateUserPrivilegeDto } from "./dto/create-user-privilege.dto";
 import { UpdateUserPrivilegeDto } from "./dto/update-user-privilege.dto";
-import { ApiBody, ApiTags } from "@nestjs/swagger";
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { UserPrivilege } from "./entities/user-privilege.entity";
 
 @ApiTags("User Privileges")
 @Controller({ version: "1", path: "user-privileges" })
@@ -19,6 +20,8 @@ export class UserPrivilegesController {
 
   @Post()
   @ApiBody({ type: CreateUserPrivilegeDto, isArray: true })
+  @ApiOperation({ summary: "Create new privilege", description: "User signup" })
+  @ApiResponse({ status: 200, type: UserPrivilege, isArray: true })
   async create(@Body() createUserPrivilegeDtos: CreateUserPrivilegeDto[]) {
     const result = [];
     for (const createUserPrivilegeDto of createUserPrivilegeDtos) {
@@ -30,16 +33,34 @@ export class UserPrivilegesController {
   }
 
   @Get()
+  @ApiBody({ type: CreateUserPrivilegeDto, isArray: true })
+  @ApiOperation({
+    summary: "Get list of privileges",
+    description: "Returns a list of privileges"
+  })
+  @ApiResponse({ status: 200, type: UserPrivilege, isArray: false })
   findAll() {
     return this.userPrivilegesService.findAll();
   }
 
   @Get(":id")
+  @ApiBody({ type: CreateUserPrivilegeDto, isArray: true })
+  @ApiOperation({
+    summary: "Get one privilege",
+    description: "Returns one privilege"
+  })
+  @ApiResponse({ status: 200, type: UserPrivilege, isArray: false })
   findOne(@Param("id") id: string) {
     return this.userPrivilegesService.findOneById(+id);
   }
 
   @Patch(":id")
+  @ApiBody({ type: CreateUserPrivilegeDto, isArray: false })
+  @ApiOperation({
+    summary: "Update user privilege",
+    description: "Update user privilege"
+  })
+  @ApiResponse({ status: 200, type: UserPrivilege, isArray: true })
   update(
     @Param("id") id: string,
     @Body() updateUserPrivilegeDto: UpdateUserPrivilegeDto
@@ -48,6 +69,12 @@ export class UserPrivilegesController {
   }
 
   @Delete(":id")
+  @ApiBody({ type: CreateUserPrivilegeDto, isArray: true })
+  @ApiOperation({
+    summary: "Delete user privilege",
+    description: "Deletes one user privilege"
+  })
+  @ApiResponse({ status: 200, type: UserPrivilege, isArray: true })
   remove(@Param("id") id: string) {
     return this.userPrivilegesService.deleteOne(+id);
   }
